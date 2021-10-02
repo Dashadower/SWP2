a0e4e629656ffe5cc86c5dba15b38536993914e1
mingtian-chan <89979849+mingtian-chan@users.noreply.github.com>
2021-10-02 16:36:33 +0900

    Update mode.py
    김신영 학우님의 의견을 받아, if, elif 사이에 return함수를 넣었습니다.

--- mode.py
+++ mode.py
@@ -2,10 +2,12 @@
 def mode(seq):
     if len(set(seq)) == 1:  # 모든 원소의 값이 같아, 최빈값이 없는경우입니다. 값을 출력하지 않습니다.
         print("모든 원소가 동일합니다.")
+        return
 
     elif len(set(seq)) == len(seq): # 모든 원소의 값이 달라, 최빈값이 없는 경우입니다. 값을 출력하지 않습니다.
         print("모든 원소의 값이 다릅니다.")
-
+        return
+    
     else:  # 최빈값이 존재하여 출력을 해야하는 경우입니다.
         counter = {}
 
