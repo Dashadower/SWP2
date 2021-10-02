276ea28161d10982dd7fc9be6d50408307debcfb
mingtian-chan <89979849+mingtian-chan@users.noreply.github.com>
2021-10-02 14:41:45 +0900

    Update mode 수정.py
    else 에서 먼저 seq를 정리하고 if else로 처리해서 에러를 수정했습니다.

--- mode 수정.py
+++ mode 수정.py
@@ -1,23 +1,25 @@
 def mode(seq):
-        if len(set(seq)) == 1:  # 얘는 프린트 안 하는 값
+        if len(set(seq)) == 1:  # 얘는 프린트 안 하는 값으로 설정하여 값이 없을 때 출력하는 문제를 해결했습니다.
             print("모든 원소가 동일합니다.")
 
         else: # 얘는 프린트 하는 값
+            counter = {}
+
+            for elem in seq:
+                if elem not in counter.keys():
+                    counter[elem] = 1
+                else:
+                    counter[elem] += 1
+
             if len(set(seq)) == len(seq):
                 print("모든 원소의 갯수가 같습니다.")
-
-            else :  # else를 활용하여, 최빈값이 없을때도 값을 출력하는 문제를 해결했습니다.
-                counter = {}
-
-                for elem in seq:
-                    if elem not in counter.keys():
-                        counter[elem] = 1
-                    else:
-                        counter[elem] += 1
-
-                    maxval = max(counter.values())
-        for key, val in counter.items():
-            print(key, end=" ") # end 를 이용하여 최빈값을 한 줄에 정리했습니다.
+                for key, val in counter.items():
+                    print(key, end=" ") # end 를 이용하여 최빈값을 한 줄에 정리했습니다.
+            else:
+                maxval = max(counter.values())
+                for key, val in counter.items():
+                    if val == maxval:
+                        print(key)
 
 
-mode([2,3])
+mode([1,2,2])
