0df24ccc576d54248e27a3646bfe95bcc56c53b5
mingtian-chan <89979849+mingtian-chan@users.noreply.github.com>
2021-10-02 15:06:13 +0900

    Update mode 수정.py

--- mode 수정.py
+++ mode 수정.py
@@ -1,25 +1,23 @@
 def mode(seq):
-        if len(set(seq)) == 1:  # 얘는 프린트 안 하는 값으로 설정하여 값이 없을 때 출력하는 문제를 해결했습니다.
-            print("모든 원소가 동일합니다.")
+    if len(set(seq)) == 1:  # 모든 원소의 값이 같아, 최빈값이 없는경우입니다. 값을 출력하지 않습니다.
+        print("모든 원소가 동일합니다.")
 
-        else: # 얘는 프린트 하는 값
-            counter = {}
+    elif len(set(seq)) == len(seq): # 모든 원소의 값이 달라, 최빈값이 없는 경우입니다. 값을 출력하지 않습니다.
+        print("모든 원소의 값이 다릅니다.")
 
-            for elem in seq:
-                if elem not in counter.keys():
-                    counter[elem] = 1
-                else:
-                    counter[elem] += 1
+    else:  # 최빈값이 존재하여 출력을 해야하는 경우입니다.
+        counter = {}
 
-            if len(set(seq)) == len(seq):
-                print("모든 원소의 갯수가 같습니다.")
-                for key, val in counter.items():
-                    print(key, end=" ") # end 를 이용하여 최빈값을 한 줄에 정리했습니다.
+        for elem in seq:
+            if elem not in counter.keys():
+                counter[elem] = 1
             else:
-                maxval = max(counter.values())
-                for key, val in counter.items():
-                    if val == maxval:
-                        print(key)
+                counter[elem] += 1
+
+        maxval = max(counter.values())
+        for key, val in counter.items():
+            if val == maxval:
+                print(key, end=" ")  # 최빈값을 한 줄에 정리하여 보기 편하게 만들었습니다.
 
 
-mode([1,2,2])
+mode([1,2,3])
